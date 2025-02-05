c44f65acc2ce12d966c12fa531117b7a69a38ea1
armelsanou <32012849+armelsanou@users.noreply.github.com>
2025-02-05 13:51:04 +0100

    Update table display 2

--- dataQalityRulesV3.py
+++ dataQalityRulesV3.py
@@ -227,7 +227,7 @@ if st.session_state["rules"]:
     rules_table = [
         {
             #"Règle": expectations_mapping.get(rule["expectation_config"]["expectation_type"], rule["expectation_config"]["expectation_type"]),
-            "Règle": expectations_mapping[rule["expectation_config"]["expectation_type"]]["label"]
+            "Règle": rule["expectation_config"]["expectation_type"]]["rule"]
             "Colonne": rule["expectation_config"]["kwargs"]["column"],
             "Paramètres": rule["expectation_config"]["kwargs"],
         }
